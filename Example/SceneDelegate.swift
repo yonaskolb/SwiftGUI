a54e34ab790eefbf93df4667cfa1c1f1c2aad5d3
yonaskolb <yonaskolb@gmail.com>
2020-03-04 20:19:02 +1100

    make example editable

--- Example/SceneDelegate.swift
+++ Example/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let config = Config()
+        let config = Config(allowEditingToggle: true)
         
         let contentView = ContentView(config: config)
 
